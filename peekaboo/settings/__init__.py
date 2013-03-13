7d09c44e97355b3ab34444275c91f93fe54095a6
Peter Bengtsson <mail@peterbe.com>
2013-03-13 14:31:48 -0700

    stackato import (thanks kumar303)

--- peekaboo/settings/__init__.py
+++ peekaboo/settings/__init__.py
@@ -1,9 +1,12 @@
+import os
 from .base import *
-try:
-    from .local import *
-except ImportError, exc:
+
+if os.environ.get('VCAP_APPLICATION'):
+    # we're in Stackto land!
+    from .stackato import *
+else:
     try:
-        from .stackato import *
-    except ImportError:
+        from .local import *
+    except ImportError, exc:
         exc.args = tuple(['%s (did you rename settings/local.py-dist?)' % exc.args[0]])
         raise exc
