98df482b9b10674fabdd4e539398df3f93fe9ff0
Peter Bengtsson <mail@peterbe.com>
2014-05-08 08:24:55 -0700

    corrected recycling script

--- peekaboo/main/management/commands/recycle.py
+++ peekaboo/main/management/commands/recycle.py
@@ -2,14 +2,10 @@ import datetime
 import os
 from optparse import make_option
 
-from django.utils.timezone import utc
-from django.core.management.base import BaseCommand
-from django.conf import settings
 from django.db import transaction
+from django.core.management.base import BaseCommand
 
-from sorl.thumbnail import default
-
-from peekaboo.main.models import Visitor, VisitorCount
+from peekaboo.main.recycle import recycle_visits
 
 
 class Command(BaseCommand):
@@ -22,41 +18,16 @@ class Command(BaseCommand):
     def handle(self, dry_run=False, **options):
         verbosity = int(options['verbosity'])
         verbose = verbosity > 1
-        since = (
-            datetime.datetime.utcnow().replace(tzinfo=utc) -
-            datetime.timedelta(hours=settings.RECYCLE_MINIMUM_HOURS)
-        )
-
-        if verbose:
-            print "SINCE", repr(since)
 
         # start transaction
         transaction.enter_transaction_management()
         transaction.managed(True)
 
-        count = 0
-        for visitor in Visitor.objects.filter(created__lt=since):
-            if verbose:
-                print "DELETE", repr(visitor.get_name())
+        recycle_visits(verbose=verbose)
 
-            VisitorCount.create_from_visitor(visitor)
-            if visitor.picture:
-                if verbose:
-                    if os.path.isfile(visitor.picture.path):
-                        print "DELETING", visitor.picture.path
-                visitor.picture.delete()
-            visitor.delete()
-            count += 1
-
-        if verbose:
-            print "\nIN SUMMARY".ljust(70, '=')
-            print count, "visitor records deleted"
-            print "\n"
         if dry_run:
             transaction.rollback()
         else:
             transaction.commit()
 
         transaction.leave_transaction_management()
-
-        default.kvstore.cleanup()
