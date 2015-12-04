0e90ce9e28844da4670d850747b6a17b10284d3a
Peter Bengtsson <mail@peterbe.com>
2015-12-04 15:43:34 -0500

    get rid of test-utils

--- peekaboo/settings/__init__.py
+++ peekaboo/settings/__init__.py
@@ -1,12 +1,23 @@
+import sys
 import os
-from .base import *
+from .base import *  # NOQA
 
-if os.environ.get('VCAP_APPLICATION'):
-    # we're in Stackto land!
-    from .stackato import *
-else:
-    try:
-        from .local import *
-    except ImportError, exc:
-        exc.args = tuple(['%s (did you rename settings/local.py-dist?)' % exc.args[0]])
-        raise exc
+
+try:
+    from .local import *  # NOQA
+except ImportError, exc:
+    exc.args = tuple(
+        ['%s (did you rename settings/local.py-dist?)' % exc.args[0]]
+    )
+    raise exc
+
+
+if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    from .test import *  # NOQA
+
+    # Are you getting full benefit from django-nose?
+    if not os.getenv('REUSE_DB', 'false').lower() in ('true', '1', ''):
+        print (
+            "Note!\n\tIf you want much faster tests in local development "
+            "consider setting the REUSE_DB=1 environment variable.\n"
+        )
